cb6064cd92eb53b9c9627c47a128a4c204d49f99
shrued <shrutisara10@gmail.com>
2021-09-02 19:59:41 +0530

    Proxy fix in react

--- client/src/components/BooksContainer.js
+++ client/src/components/BooksContainer.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 export default class BooksContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("api/v1/books.json")
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          books: response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
   render() {
-    return <div className="Lists-container">Lists</div>;
+    return (
+      <div className="books-container">
+        {this.state.books.map((book) => {
+          return (
+            <div className="single-book" key={book.id}>
+              <h4>{book.title}</h4>
+              <p>{book.excerpt}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
